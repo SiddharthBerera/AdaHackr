5e76c376d07eb013bfcacd50e412c64a32b1b33d
Siddharth Berera <s2020550@ed.ac.uk>
2021-10-16 14:26:39 +0100

    Add files via upload
    upadated data_stream.py file, streams btc/usd value and both stores info (every min) in arrays and writes it to a csv file.

--- data_stream.py
+++ data_stream.py
@@ -3,6 +3,7 @@ import json
 import pprint
 import time
 from datetime import datetime
+import csv
 
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
@@ -27,20 +28,42 @@ times = []
 prices = []
 #start time
 start_at = datetime.now()
-start_time = start_at.strftime("%H:%M:%S")
+start_time = start_at.strftime("%H:%M")
 print("Start Time =", start_time)
+
+#create data_file
+fieldnames = ["minute", "time", "price"]
+with open('data.csv', 'w') as csv_file:
+    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    csv_writer.writeheader()
+
 while True:
     #get time asap (nano second delays)
     now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
+    current_time = now.strftime("%H:%M")
 
     #get price next (time isn't as of the essence here since cmc noob api only updates price every min)
-    prices.append(json.loads(response.text)['data']['BTC']['quote']['USD']['price'])
+    prices.append(json.loads(response.text)['BTC']['quote']['US['data']D']['price'])
     
     #print price (testing purposes)
     print("Current Time =", current_time)
     print(prices)
-    times.append(i)
+    times.append(current_time)
+    print(times)
+
+    info = {
+            "minute": i,
+            "time": times[i],
+            "price": prices[i]
+        }
+    
+    #open file to write minute, time, price
+    with open('data.csv', 'a') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        csv_writer.writerow(info)
+    
+        #increment minute counter and get data in 60 seconds (noob api version)
     i+=1
     time.sleep(60)
+
 
